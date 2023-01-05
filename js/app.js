7f411bf355d218b6f9503042a97e98d75f1319cc
H Brown <hebrown12@gmail.com>
2023-01-05 15:00:21 -0600

    working on functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ class Game {
     constructor() {
         this.gameBtn = document.getElementById('gameBtn')
         this.tradeBtn = document.getElementById('tradeBtn')
+        this.rollBtn = document.getElementById('rollBtn')
         this.redCubes = document.getElementById('redCubes')
         this.blueCubes = document.getElementById('blueCubes')
         this.greenCubes = document.getElementById('greenCubes')
@@ -9,6 +10,8 @@ class Game {
         this.goldCube = document.getElementById('goldCube')
         this.tradingValue = document.getElementById('tradingValue')
         this.dieRoll = document.getElementById('dieRoll')
+
+        this.tradeValue
 
         this.cubes = {
             red: 0,
@@ -21,17 +24,82 @@ class Game {
 
     init() {
         this.setTradeValue()
+
     }
 
     setTradeValue() {
         const gameBtn = this.gameBtn
-        console.log(gameBtn)
+
         gameBtn.addEventListener('click', ()=> {
             // console.log('click')
-            const tradeValue = Math.ceil(Math.random() * 6)
-            this.tradingValue.innerText = tradeValue
+            this.tradeValue = Math.floor(Math.random() * (6 - 2 + 1) + 2)
+            this.tradingValue.innerText = this.tradeValue
+            gameBtn.classList.add('disabled')
+
+            this.rollDie()
+            
         })
     }
+
+    rollDie() {
+        const rollBtn = this.rollBtn
+        const dieRoll = this.dieRoll
+
+        if (this.tradeValue > 0) {
+            rollBtn.addEventListener('click', ()=> {
+                let roll = Math.ceil(Math.random() * 6)
+                dieRoll.innerText = roll
+                this.buildRedCubes(roll)
+                this.cubes.red+= roll
+                // console.log(this.cubes)
+            })
+        }
+    }
+
+    buildRedCubes(cubes) {
+        for (let i = 0; i < cubes; i++) {
+            
+            const redCube = document.createElement('div')
+            redCube.classList.add('cube', 'red')
+            this.redCubes.append(redCube)
+        }
+    }
+
+    buildCube(color) {
+        const cube = document.createElement('div')
+        cube.classList.add('cube', color)
+        
+        switch (color) {
+            case 'blue': 
+                this.blueCubes.append(cube)
+                break
+            case 'green':
+                this.greenCubes.append(cube)
+                break
+            case 'purple':
+                this.purpleCubes.append(cube)
+                break
+            case 'gold':
+                this.goldCube.append(cube)
+                break
+            default:
+                break
+        }
+    }
+
+    tradeCubes(tradeValue, prevColor, nextColor) {
+        const tradeBtn = this.tradeBtn
+
+        tradeBtn.addEventListener('click', ()=> {
+            if (tradeValue >= this.cubes[prevColor]) {
+                this.buildCube(nextColor)
+            }
+
+        })
+
+    }
+
+
 }
 
 const action = new Game()
